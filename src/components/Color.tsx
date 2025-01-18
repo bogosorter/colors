2ca15faeeb78e36b741667f04d7eb075093a48d4
bogosorter <luiswbarbosa@gmail.com>
2025-01-18 15:14:55 +0000

    added click-to-copy

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -2,7 +2,12 @@ import { CSSProperties } from "react";
 import './Color.css';
 
 export default function Color({ name, hex }: { name: string, hex: string }) {
-    return <div className='color'>
+
+    function copyToClipboard() {
+        navigator.clipboard.writeText(hex);
+    }
+
+    return <div className='color' onClick={copyToClipboard}>
         <div className='showcase' style={createShowcaseStyle(hex)}></div>
         <div className='name'>{name}</div>
         <div className='hex'>{hex}</div>
